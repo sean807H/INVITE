b70309c6fa03172e5887672679699cd779dffbbe
김서연 <ksyssyy070814@gmail.com>
2024-10-29 17:04:08 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        Character character = new Character("릴리안");
+        Story story = new Story();
+        story.startStory(character);
+
+        GameUI gameUI = new GameUI();
+        gameUI.showCharacterName(character);
+    }
+}
+
+// Character.java: 캐릭터 이름을 저장하고 반환하는 클래스입니다.
+// Story.java: 캐릭터의 이야기를 시작하는 메서드가 있는 클래스입니다.
+// GameUI.java: 캐릭터 이름을 화면에 표시하는 사용자 인터페이스 클래스입니다.
+// Main.java: 프로그램의 진입점으로, 캐릭터를 생성하고 이야기를 시작하며 UI를 보여줍니다.
