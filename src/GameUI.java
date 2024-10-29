18bf7614cfbd71c5f5bd3e61ac693c6550ac3dcd
김서연 <ksyssyy070814@gmail.com>
2024-10-29 18:23:35 +0900

    Resolved merge conflicts

--- src/GameUI.java
+++ src/GameUI.java
@@ -6,7 +6,7 @@ public class GameUI {
         JFrame frame = new JFrame("Game");
         JLabel label = new JLabel("캐릭터 이름: " + character.getName());
         frame.add(label);
-        frame.setSize(300, 200);
+        frame.setSize(300, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
